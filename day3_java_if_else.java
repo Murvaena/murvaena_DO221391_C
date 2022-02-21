30833c09feeafdd91add37db91256e8431c37a3f
Murvaena <99964711+Murvaena@users.noreply.github.com>
2022-02-21 22:49:55 +0800

    day3_java_if_else.java

---
+++ day3_java_if_else.java
@@ -0,0 +1,13 @@
+if(N%2==1){//ODD
+            System.out.println("weird");    
+        }else{//EVEN
+            if(2 <=N && N<=5){
+                System.out.println("Not weird"); 
+            }else if(6 <=N && N<=20){ 
+                System.out.println("weird");  
+            }else if(N>20){ 
+                System.out.println("not weird"); 
+            }            
+        }
+    }       
+}     
