3b6585c66b3941318d0f8fde684cec0ddabc282e
murvaena <99964711+Murvaena@users.noreply.github.com>
2022-03-20 22:20:41 +0800

    day30_jav_subarray.java

---
+++ day30_jav_subarray.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[]A = new int[n];
+        for(int i=0;i<n;i++){
+            A[i]=scanner.nextInt();
+        }
+        
+        int nNegativeSum = 0;
+       for(int i=0;i<A.length;i++){//start
+           for(int j=i;j<A.length;j++){//end 
+               int sum = 0;
+               int t = 0;
+               for(int k=i;k<=j;k++){
+                    sum += A[k];
+               }                 
+               if(sum<0){
+                   nNegativeSum++;
+               }
+               //System.out.println(" = "+sum);
+           }
+        }   
+    System.out.println(nNegativeSum);         
+    }              
+            
+}
